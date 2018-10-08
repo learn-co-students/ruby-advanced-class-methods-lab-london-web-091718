31c9e29f57fb53e6073e812fb19412c57be91844
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-08 09:05:01 +0100

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -6,8 +7,65 @@ class Song
     @@all
   end
 
+  def self.create
+    song = Song.new
+    @@all << song
+    song
+  end
+
   def save
     self.class.all << self
   end
 
+  def self.new_by_name(song_n)
+    song = self.new
+    song.name = song_n
+    song.save
+    song
+  end
+
+  def self.create_by_name(song_n)
+    song = self.create
+    song.name = song_n
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|songs| songs.name == name ? songs : false}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) == nil ? self.create_by_name(name) : self.find_by_name(name)
+
+    # if self.find_by_name(name)
+    #   self.find_by_name(name)
+    # else
+    #   self.create_by_name(name)
+    # end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.new_from_filename(filename)
+    filename = filename.chomp(".mp3")
+    split_filename = filename.split(" - ")
+    song = self.new
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    filename = filename.chomp(".mp3")
+    split_filename = filename.split(" - ")
+    song = self.create_by_name(split_filename[1])
+    song.artist_name = split_filename[0]
+    song
+  end
 end
